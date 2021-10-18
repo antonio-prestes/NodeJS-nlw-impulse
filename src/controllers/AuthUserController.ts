ad6a8d8b9c84f8de3c22a4b8ebbacd07d71ce226
Diego <diegoantoniodasp@hotmail.com>
2021-10-18 20:25:07 -0300

    [NLW Heat] Mission: Impulse – Stage 1 59:44

---
+++ src/controllers/AuthUserController.ts
@@ -0,0 +1,15 @@
+import {json, Request, Response} from "express";
+import {AuthUserService} from "../services/AuthUserService";
+
+class AuthUserController {
+    async handle(request: Request, response: Response) {
+        const {code} = request.body;
+
+        const service = new AuthUserService()
+        const result = await service.execute(code);
+
+        return  response.json(result);
+    }
+}
+
+export {AuthUserController}
---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import {Router} from "express";
+import {AuthUserController} from "./controllers/AuthUserController";
+
+const router = Router();
+
+router.post('/auth', new AuthUserController().handle);
+
+export {router}
