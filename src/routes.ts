931eab575a164f9704c4e7a4b68f59a1d3cb33c0
Diego <diegoantoniodasp@hotmail.com>
2021-10-18 22:12:18 -0300

    [NLW Heat] Mission: Impulse – Stage 1 1:35:35

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import {Router} from "express";
 import {AuthUserController} from "./controllers/AuthUserController";
+import {ensureAuth} from "./middleware/ensureAuth";
+import {CreateMessageController} from "./controllers/CreateMessageController";
 
 const router = Router();
 
 router.post('/auth', new AuthUserController().handle);
+
+router.post('/messages', ensureAuth, new CreateMessageController().handle);
 
 export {router}
