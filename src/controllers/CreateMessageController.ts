5664cc937826dde8576554e8e95ded80f0a9cd08
Diego <diegoantoniodasp@hotmail.com>
2021-10-18 22:53:52 -0300

    [NLW Heat] Mission: Impulse – Stage 1 Finalizada

--- src/controllers/CreateMessageController.ts
+++ src/controllers/CreateMessageController.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express";
 import {CreateMessageService} from "../services/CreateMessageService";
 
+
 class CreateMessageController {
     async handle(request: Request, response: Response) {
         const {message} = request.body;
@@ -11,6 +12,7 @@ class CreateMessageController {
         const result = await service.execute(message, user_id);
 
         return response.json(result)
+
     }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,17 @@ import {Router} from "express";
 import {AuthUserController} from "./controllers/AuthUserController";
 import {ensureAuth} from "./middleware/ensureAuth";
 import {CreateMessageController} from "./controllers/CreateMessageController";
+import {GetLast3MessagesController} from "./controllers/GetLast3MessagesController";
+import {ProfileUserController} from "./controllers/ProfileUserController";
 
 const router = Router();
 
 router.post('/auth', new AuthUserController().handle);
 
 router.post('/messages', ensureAuth, new CreateMessageController().handle);
+
+router.get('/messages/last3', new GetLast3MessagesController().handle);
+
+router.get('/profile',ensureAuth, new ProfileUserController().handle);
 
 export {router}
